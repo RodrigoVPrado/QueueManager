ade8d9d9782e7807bdd69718d4939d77c95c3470
RodrigoVPrado <pradorodrigo19@gmail.com>
2022-06-06 20:46:49 -0300

    Fila prioritaria Refaroração do main

--- src/main/main.java
+++ src/main/main.java
@@ -2,21 +2,64 @@ package main;
 
 import java.util.Scanner;
 
-import main.domain.QueueManagement;
+import main.domain.CicleQueueManagement;
+import main.domain.PrioritayQueueManagement;
+import main.exception.EmptyQueueException;
 
 public class main {
 
 	public static void main(String[] args) throws Exception {
-		
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Selecione a fila que quer executar (circular/prioridade)");
+		final var decision = scanner.next();
+		if ("circular".equalsIgnoreCase(decision))
+			circularQueueInititalizer(scanner);
+		else
+			prioritaryQueueInititalizer(scanner);
+
+	}
+
+	private static void prioritaryQueueInititalizer(Scanner scanner) throws Exception{
+		System.out.println("insira o tamanho da fila");
+		final var size = scanner.nextInt();
+		String[] prioritayQueue = new String[size];
+		PrioritayQueueManagement prioritayQueueManagement = new PrioritayQueueManagement(prioritayQueue);
+
+		while (true) {
+			if (prioritayQueueManagement.getQueue().length > 1) {
+				System.out.println("Deseja adicionar ou remover um elemento da fila?(Prioridade/Adicionar/Remover/N)");
+			} else {
+				System.out.println("Deseja adicionar na fila?(Adicionar/N)");
+			}
+			String decision = scanner.next();
+			if ("n".equalsIgnoreCase(decision)) {
+				break;
+
+			} else if ("adicionar".equalsIgnoreCase(decision)) {
+				System.out.println("digite o valor da fila");
+				prioritayQueueManagement.addQueue(scanner.next());
+
+			} else if ("prioridade".equalsIgnoreCase(decision)){
+				System.out.println("digite o valor da fila");
+				prioritayQueueManagement.addPrioritayQueue(scanner.next());
+
+			} else if("remover".equalsIgnoreCase(decision)) {
+				prioritayQueueManagement.removePrioritayQueue();
+
+			}
+		}
+	}
+
+	private static void circularQueueInititalizer(Scanner scanner) throws EmptyQueueException {
+
 		System.out.println("insira o tamanho da fila");
 		final var size = scanner.nextInt();
 		String[] filaCiruclar = new String[size];
-		QueueManagement queueManagement = new QueueManagement(filaCiruclar);
+		CicleQueueManagement cicleQueueManagement = new CicleQueueManagement(filaCiruclar);
 
 		while (true) {
-			if (queueManagement.getQueue().length > 1) {
-				System.out.println("Deseja adicionar ou remover um elemento da fila?(Adicionar/remover/N)");
+			if (cicleQueueManagement.getQueue().length > 1) {
+				System.out.println("Deseja adicionar ou remover um elemento da fila?(Adicionar/Remover/N)");
 			} else {
 				System.out.println("Deseja adicionar na fila?(Adicionar/N)");
 			}
@@ -25,10 +68,10 @@ public class main {
 				break;
 			} else if ("adicionar".equalsIgnoreCase(decision)) {
 					System.out.println("digite o valor da fila");
-					queueManagement.addCicleQueue(scanner.next());
+					cicleQueueManagement.addCicleQueue(scanner.next());
 					 
 			} else if("remover".equalsIgnoreCase(decision)) {
-				queueManagement.removeCicleQueue();
+				cicleQueueManagement.removeCicleQueue();
 
 			}
 
